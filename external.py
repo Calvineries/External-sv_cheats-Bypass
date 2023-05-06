14bae533f7c8d2a653cfb115bb742529c8c7f248
Calvineries <Calvineries@gmail.com>
2023-05-06 18:34:36 +0200

    fix mistake

--- external.py
+++ external.py
@@ -75,6 +75,14 @@ def game_selection(sender, data):
         sv_cheatsaddress = 0x576178
         r_drawothermodelsaddress = 0x53C8C8
 
+    if game == "DODS":
+        pm = pymem.Pymem('hl2.exe')
+        client = pymem.pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
+        engine = pymem.pymem.process.module_from_name(pm.process_handle, 'engine.dll').lpBaseOfDll
+        #Day Of Defeat Source Address
+        sv_cheatsaddress = 0x616A70
+        r_drawothermodelsaddress = 0x4A0C20
+
     if game == "Portal2":
         pm = pymem.Pymem('portal2.exe')
         client = pymem.pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
@@ -120,7 +128,7 @@ with gui.window(label='Nexus', width=350, height=400, no_title_bar=True, no_resi
         with gui.tab(label='Game Selection'):
             gui.add_text("Select the game :")
             gui.add_text("(The game must already be launched)")
-            gui.add_combo(("CS:GO", "GMOD (default branch)", "GMOD (x64)", "TF2", "CSS", "L4D2", "L4D1", "Portal2"), callback=game_selection, tag="SelectGameCombo")
+            gui.add_combo(("CS:GO", "GMOD (default branch)", "GMOD (x64)", "TF2", "CSS", "L4D2", "L4D1", "DODS", "Portal2"), callback=game_selection, tag="SelectGameCombo")
 
         with gui.tab(label=game, show=False, tag="ShowGameTab"):
             gui.add_checkbox(label='sv_cheats', callback=checkbox_svcheats)
@@ -129,7 +137,7 @@ with gui.window(label='Nexus', width=350, height=400, no_title_bar=True, no_resi
                 gui.add_checkbox(label='r_drawothermodels', callback=checkbox_drawmodels)
 
         with gui.tab(label="About"):
-            gui.add_text("Version : 1.0.6")
+            gui.add_text("Version : 1.0.7")
             # https://github.com/Calvineries/External-sv_cheats-Bypass
             gui.add_text("GitHub Page : github.com/Calvineries\n/External-sv_cheats-Bypass")            
             gui.add_text("")
