ca165e5304c15ed09e5bb261fac0752b8b7a2bf5
Calvineries <Calvineries@gmail.com>
2023-05-06 18:32:04 +0200

    Code optimization

--- external.py
+++ external.py
@@ -14,6 +14,11 @@ def game_selection(sender, data):
     global sv_cheatsaddress
     global r_drawothermodelsaddress
     game = gui.get_value(sender)
+
+    gui.show_item("ShowGameTab")
+    gui.set_item_label("ShowGameTab", game)
+    gui.disable_item("SelectGameCombo")
+
     if game == "TF2":
         pm = pymem.Pymem('hl2.exe')
         client = pymem.pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
@@ -21,10 +26,6 @@ def game_selection(sender, data):
         #TF2 Address
         sv_cheatsaddress = 0x606160
         r_drawothermodelsaddress = 0xC44540
-
-        gui.show_item("ShowGameTab")
-        gui.set_item_label("ShowGameTab", game)
-        gui.disable_item("SelectGameCombo")
 
     if game == "CS:GO":
         pm = pymem.Pymem('csgo.exe')
@@ -34,10 +35,6 @@ def game_selection(sender, data):
         sv_cheatsaddress = 0x5A4268
         r_drawothermodelsaddress = 0xDE5648
 
-        gui.show_item("ShowGameTab")
-        gui.set_item_label("ShowGameTab", game)
-        gui.disable_item("SelectGameCombo")
-
     if game == "CSS":
         pm = pymem.Pymem('hl2.exe')
         client = pymem.pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
@@ -46,10 +43,6 @@ def game_selection(sender, data):
         sv_cheatsaddress = 0x616A70
         r_drawothermodelsaddress = 0x4C4BA0
 
-        gui.show_item("ShowGameTab")
-        gui.set_item_label("ShowGameTab", game)
-        gui.disable_item("SelectGameCombo")
-
     if game == "GMOD (default branch)":
         pm = pymem.Pymem('hl2.exe')
         client = pymem.pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
@@ -57,22 +50,14 @@ def game_selection(sender, data):
         #GMOD Address
         sv_cheatsaddress = 0x69B6A8
         r_drawothermodelsaddress = 0x6EB2A0
-
-        gui.show_item("ShowGameTab")
-        gui.set_item_label("ShowGameTab", game)
-        gui.disable_item("SelectGameCombo")
         
     if game == "GMOD (x64)":
         pm = pymem.Pymem('gmod.exe')
         client = pymem.pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
         engine = pymem.pymem.process.module_from_name(pm.process_handle, 'engine.dll').lpBaseOfDll
-        #GMOD Address
+        #GMOD x64 Address
         sv_cheatsaddress = 0x7BD318
         r_drawothermodelsaddress = 0x83A998
-
-        gui.show_item("ShowGameTab")
-        gui.set_item_label("ShowGameTab", game)
-        gui.disable_item("SelectGameCombo")
 
     if game == "L4D2":
         pm = pymem.Pymem('left4dead2.exe')
@@ -82,10 +67,6 @@ def game_selection(sender, data):
         sv_cheatsaddress = 0x6719A0
         r_drawothermodelsaddress = 0x7B110768
 
-        gui.show_item("ShowGameTab")
-        gui.set_item_label("ShowGameTab", game)
-        gui.disable_item("SelectGameCombo")
-
     if game == "L4D1":
         pm = pymem.Pymem('left4dead.exe')
         client = pymem.pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
@@ -94,22 +75,6 @@ def game_selection(sender, data):
         sv_cheatsaddress = 0x576178
         r_drawothermodelsaddress = 0x53C8C8
 
-        gui.show_item("ShowGameTab")
-        gui.set_item_label("ShowGameTab", game)
-        gui.disable_item("SelectGameCombo")
-        
-    if game == "DODS":
-        pm = pymem.Pymem('hl2.exe')
-        client = pymem.pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
-        engine = pymem.pymem.process.module_from_name(pm.process_handle, 'engine.dll').lpBaseOfDll
-        #Day Of Defeat Source Address
-        sv_cheatsaddress = 0x616A70
-        r_drawothermodelsaddress = 0x4A0C20
-
-        gui.show_item("ShowGameTab")
-        gui.set_item_label("ShowGameTab", game)
-        gui.disable_item("SelectGameCombo")
-
     if game == "Portal2":
         pm = pymem.Pymem('portal2.exe')
         client = pymem.pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
@@ -117,10 +82,6 @@ def game_selection(sender, data):
         #Portal2 Address
         sv_cheatsaddress = 0x66B778
         r_drawothermodelsaddress = 0x9962C8
-
-        gui.show_item("ShowGameTab")
-        gui.set_item_label("ShowGameTab", game)
-        gui.disable_item("SelectGameCombo")
 
 
 #Checkbox sv_cheats
@@ -159,7 +120,7 @@ with gui.window(label='Nexus', width=350, height=400, no_title_bar=True, no_resi
         with gui.tab(label='Game Selection'):
             gui.add_text("Select the game :")
             gui.add_text("(The game must already be launched)")
-            gui.add_combo(("CS:GO", "GMOD (default branch)", "GMOD (x64)", "TF2", "CSS", "L4D2", "L4D1", "DODS", "Portal2"), callback=game_selection, tag="SelectGameCombo")
+            gui.add_combo(("CS:GO", "GMOD (default branch)", "GMOD (x64)", "TF2", "CSS", "L4D2", "L4D1", "Portal2"), callback=game_selection, tag="SelectGameCombo")
 
         with gui.tab(label=game, show=False, tag="ShowGameTab"):
             gui.add_checkbox(label='sv_cheats', callback=checkbox_svcheats)
@@ -169,7 +130,8 @@ with gui.window(label='Nexus', width=350, height=400, no_title_bar=True, no_resi
 
         with gui.tab(label="About"):
             gui.add_text("Version : 1.0.6")
-            gui.add_text("GitHub Page : github.com/Calvineries/External-sv_cheats-Bypass")
+            # https://github.com/Calvineries/External-sv_cheats-Bypass
+            gui.add_text("GitHub Page : github.com/Calvineries\n/External-sv_cheats-Bypass")            
             gui.add_text("")
             gui.add_text("Author : Calvineries")
             gui.add_text("Contributors: ...")
